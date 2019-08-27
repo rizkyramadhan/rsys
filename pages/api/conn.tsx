505addf77f268dc902c4eaa02305d12305d50b39
Nizam Miftahul <nizamiftahul@gmail.com>
2019-08-27 15:19:08 +0700

    if connection open

--- pages/api/conn.tsx
+++ pages/api/conn.tsx
@@ -1,16 +1,19 @@
 const config = require("../../config.js");
 const db = config._instance.db;
 let knex = require("knex");
+let check = undefined;
 
 export const conn = (c: string) => {
   const db_host = db[c] || db["default"];
-  return knex({
+  if (check) return check;
+  return (check = knex({
     client: db_host.driver,
     connection: {
       host: db_host.host,
       user: db_host.username,
       password: db_host.password,
       database: db_host.database
-    }
-  });
+    },
+    debug: true
+  }));
 };

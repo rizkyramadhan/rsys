d7db7148be82e666147e3ca744789f35db92b941
Riz <riz@rizbook.local>
2019-08-28 11:42:18 +0700

    remove uneeded file

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "06c3c396d7905bde0161";
+/******/ 	var hotCurrentHash = "66a1a85ffa64d577d97d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
